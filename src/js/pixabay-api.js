3e7fc27f01dd8487a8ff4dde2d341e121c2886f5
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-04-12 08:28:11 +0300

    add loader and form

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,16 @@
+const KEY_API = '43330022-ba36650dc11c5492edb18bb80'
+const BASE_URL = 'https://pixabay.com'
+
 export function fetchPhotoByPixaby(searchWord) {
-    return fetch(`https://pixabay.com/api/?key=43330022-ba36650dc11c5492edb18bb80&image_type=photo&orientation=horizontal&safesearch=true&q=${searchWord}`).then(res => {
+    const searchParams = new URLSearchParams({
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        key: KEY_API,
+        q: searchWord
+    })
+
+    return fetch(`${BASE_URL}/api/?${searchParams}`).then(res => {
         if(!res.ok) {
             throw new Error(res.status)
         }
--- src/main.js
+++ src/main.js
@@ -1,20 +1,33 @@
 import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
 
 import { fetchPhotoByPixaby } from "./js/pixabay-api";
+import { createCard } from "./js/render-functions";
+
 
 const formElem = document.querySelector('.form')
 const inputFormElem = document.querySelector('.form-input')
-const formBtnElem = document.querySelector('.form-btn')
+const galleryListElem = document.querySelector('.gallery-list')
+const loaderElem = document.querySelector('.loader')
 
 function inputSearch(event) {
     event.preventDefault();
 
     if (inputFormElem.value.trim() === '') {
+        iziToast.info({
+            message: 'Введіть слово в поле для пошуку',
+            position: 'topRight',
+            backgroundColor: 'orange',
+        })
+        galleryListElem.innerHTML = ''
+
         return
     }
+    loaderElem.classList.add('is-visible')
 
     fetchPhotoByPixaby(inputFormElem.value)
+        .finally(() => loaderElem.classList.remove('is-visible'))
+
         .then(data => {
             if (!data.hits.length) {
                 iziToast.error({
@@ -24,12 +37,25 @@ function inputSearch(event) {
                     theme: 'dark',
                     position: 'topRight',
                 });
+                return
             }
-            console.log(data.hits)
+
+            galleryListElem.innerHTML = createCard(data.hits)
         })
-        .catch (error => console.log(error))
+        .catch(error => console.log(error))
+
 
     inputFormElem.value = ''
 }
 formElem.addEventListener('submit', inputSearch)
 
+
+let gallery = new SimpleLightbox('.gallery-item__link', {
+    captionsData: 'alt',
+    captionDelay: 250
+}
+);
+
+gallery.on('show.simplelightbox', function (event) {
+    event.preventDefault()
+});
